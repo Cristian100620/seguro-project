91a7bdf4d5bb7455c868f64dd863279ea53ac463
cristiandaniel2002e <cristiandaniel2002e@gmail.com>
2024-06-06 20:30:09 -0400

    segunda parcial

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { initializeAppCheck, ReCaptchaEnterpriseProvider, provideAppCheck } from '@angular/fire/app-check';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
+import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai-preview';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+     provideFirebaseApp(() => 
+      initializeApp({
+        apiKey: "AIzaSyAKMuullUyw1TEV6G7C6X0K6_VQFghSkUo",
+        authDomain: "lpvionic.firebaseapp.com",
+        projectId: "lpvionic",
+        storageBucket: "lpvionic.appspot.com",
+        messagingSenderId: "40647760359",
+        appId: "1:40647760359:web:9d7e0fab03c74762064542",
+        measurementId: "G-E70ZXEZPTT"
+    })
+  ), 
+  provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()),
+      ScreenTrackingService, UserTrackingService, provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideFunctions(() => getFunctions()), provideMessaging(() => getMessaging()), providePerformance(() => getPerformance()), provideStorage(() => getStorage()), provideRemoteConfig(() => getRemoteConfig()), provideVertexAI(() => getVertexAI())],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/cliente/cliente-edit/cliente-edit.page.ts
@@ -0,0 +1,114 @@
+import { Component, OnInit } from '@angular/core';
+import { collection, addDoc, updateDoc, getDoc, Firestore, doc, } from '@angular/fire/firestore';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-cliente-edit',
+  templateUrl: './cliente-edit.page.html',
+  styleUrls: ['./cliente-edit.page.scss'],
+})
+export class ClienteEditPage implements OnInit {
+
+  id: any;
+  cliente: any = [];
+  constructor(
+    private readonly firestore: Firestore,
+    private route: ActivatedRoute,
+    private rt: Router
+  ) { }
+
+  ngOnInit() {
+
+    this.route.params.subscribe((params: any) => {
+      //console.log('params', params);
+      this.id = params.id;
+      //console.log('id', this.id);
+      if (this.id) {
+        this.obtenerCliente(this.id);
+      }
+
+    });
+  }
+
+  volver = () => {
+    this.rt.navigate(['/cliente-list']);
+
+
+  }
+  accion = (id: string) => {
+    if (this.id) {
+      //console.log("modificar");
+      this.editarCliente(this.id);
+    
+    } else {
+      //console.log("guardar");
+      this.incluirCliente();
+
+    }
+    this.volver();
+  }
+
+  incluirCliente = () => {
+    console.log('aqui incluir en firebase');
+    let alumnaRef = collection(this.firestore, 'cliente');
+
+    addDoc(
+      alumnaRef,
+      {
+        nombre: (this.cliente.nombre)?(this.cliente.nombre):"",
+        apellido: (this.cliente.apellido)?(this.cliente.apellido):"",
+        nacimiento: (this.cliente.nacimiento)?(new Date(this.cliente.nacimiento)):new Date(),
+        bien_asegurado: (this.cliente.bienAsegurado)?(this.cliente.bien_asegurado):"",
+        monto_asegurado: (this.cliente.montoAsegurado)?(this.cliente.bien_asegurado):"",
+
+      }
+
+    ).then(doc => {
+      console.log('registro incluido');
+      this.volver();
+
+    }
+    );
+  }
+
+  editarCliente = (id: string) => {
+    console.log('aqui editar en firebase');
+    const document = doc(this.firestore, 'cliente', this.id);
+
+    updateDoc(
+      document,
+      {
+        nombre: (this.cliente.nombre)?(this.cliente.nombre):"",
+        apellido: (this.cliente.apellido)?(this.cliente.apellido):"",
+        nacimiento: (this.cliente.nacimiento)?(new Date(this.cliente.nacimiento)):new Date(),
+        bien_asegurado: (this.cliente.bien_asegurado)?(this.cliente.bien_asegurado):"",
+        monto_asegurado: (this.cliente.monto_asegurado)?(this.cliente.monto_asegurado):"",
+        
+      }
+
+    ).then(doc => {
+      console.log('registro editado');
+      this.volver();
+
+    }
+    );
+  }
+
+  obtenerCliente = (id: string) => {
+
+    const document = doc(this.firestore, 'cliente', id);
+
+    
+
+    getDoc(document).then(doc => {
+      console.log('registro a editar', doc.data());
+      this.cliente = doc.data();
+      const timestamp = this.cliente.nacimiento; // Asume que 'fecha' es el campo Timestamp
+      this.cliente.nacimiento = timestamp.toDate().toISOString(); // Convierte a ISO 8601
+      
+    }
+    );
+  }
+  
+
+}
